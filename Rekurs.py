f0eca821f1ce2a475d59dc44bc5111a0dbd006a5
Jerolock <163981038+Jerolock@users.noreply.github.com>
2024-04-14 11:26:37 +0500

    Update Rekurs.py

--- Rekurs.py
+++ Rekurs.py
@@ -1,8 +1,15 @@
-def test(n):
+def test(*params):
+    print(*params)
+
+
+test(1, 2, 3, 4, 'str', True)
+
+
+def rekurs(n):
     if n == 1:
         return 1
     else:
-        return n * test(n - 1)
+        return n * rekurs(n - 1)
 
 
-print(test(14))
+print(rekurs(14))
